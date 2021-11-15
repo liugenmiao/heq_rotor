2c7a6732f5240283e68147ffd8a884ad8b102d14
liugenmiao <liugenmiaofirst@163.com>
2021-11-15 02:41:56 -0800

    去除因为git版本号检查，导致编译错误

--- src/lib/version/px_update_git_header.py
+++ src/lib/version/px_update_git_header.py
@@ -72,7 +72,7 @@ if validate:
         print("  v1.9.0-1.0.0")
         print("See also https://dev.px4.io/master/en/setup/building_px4.html#firmware_version")
         print("")
-        sys.exit(1)
+        #sys.exit(1)
 
 git_version = subprocess.check_output('git rev-parse --verify HEAD'.split(),
                                       stderr=subprocess.STDOUT).decode('utf-8').strip()
